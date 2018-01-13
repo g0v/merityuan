9b4802524489eaa5d373f6d777f7dec9f8edde20
YuTin <yuting1987@gmail.com>
2018-01-13 10:16:26 +0800

    refactor(web): init project

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  exportPathMap: () => ({
+    '/': { page: '/' },
+  }),
+  webpack: (config) => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = {
+      fs: 'empty'
+    }
+
+    return config
+  }
+}
